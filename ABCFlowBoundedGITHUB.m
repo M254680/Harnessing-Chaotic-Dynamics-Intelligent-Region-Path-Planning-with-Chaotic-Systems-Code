33dea572fc32123bd20b565f93af21eab0e9e6e6
M254680 <165702740+M254680@users.noreply.github.com>
2024-04-06 20:41:31 -0400

    Update and rename ABCFlowBoundedMK2.m to ABCFlowBoundedGITHUB.m

--- ABCFlowBoundedGITHUB.m
+++ ABCFlowBoundedGITHUB.m
@@ -1,49 +1,33 @@
 %%
-
-%
 clear all, clc, close all
 
-%Defining Variables
-% xmax=5;
-% xmin=0;
-% ymax=5;
-% ymin=0;
+%Defining Limits
+ xmax=5;
+ xmin=0;
+ ymax=5;
+ymin=0;
 
-
+%defining parameters
 A=1;
 B=0.5;
 C=.5;
 nu=1;
-
 tspan=0:.01:50;
-
-%Arnold Equation
-%X0_1 = [0  1 0 2.5 2.5 ];
-
 X0_1 = [-3 0 1 2.5 2.5 ]; %in river %time series goes to constant
 %X0_1 = [1  0 0 2.5 2.5 ]; %in circle Time series still chaotic in X1 X2 not
 %X0_1 = [0  0 -1 2.5 2.5 ]; %in sea Time series still chaotic
-%X0_1 = [0 0 -1 0 0 ];
 
+%Arnold Equation
 Arnold =@(t,X)[ A*sin(X(3))+C*cos(X(2));
     B*sin(X(1))+A*cos(X(3));
     C*sin(X(2))+B*cos(X(1));
     nu*cos(X(3));
     nu*sin(X(3))
     ];
-
+%Solving ODE
 [t1, X1] = ode45(Arnold, tspan, X0_1);
 
-%%
-% %sanity check
-% plot(t1,X1(:,1))
-% figure
-% plot(X1(:,1),X1(:,3))
-% title('Pointcare Section for in sea','FontSize',14)
-% ylim([-3,3])
-% xlim([-3,3])
-% ylabel('x3','FontSize',14)
-% xlabel('x1','FontSize',14)
+
 
  % Bounding Method
 for k = 1:length(t1)
@@ -102,20 +86,16 @@ end
 
 
 %plotting
-    %plot(X1(k,4), X1(k,5), '.','MarkerSize', 15,'Color', [0 0 0], 'LineWidth', 1);
-
-
-
 c1 = X1(:,4) ;
 c2 = X1(:,5) ;
 
 c1(end+1) = NaN ;
 c2(end+1) = NaN;
-%c=1:length(c1);
+
 c=ones(length(c1),1);
 
 figure
-%patch(c1,c2,c,'EdgeColor','interp','Marker','.','MarkerFaceColor','flat','MarkerSize', 15);
+
 patch(c1,c2,c,'EdgeColor','interp','Marker','.');
      xlabel('X', 'FontSize', 14);
     ylabel('Y', 'FontSize', 14);
